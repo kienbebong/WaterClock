2f2c7cc0712cb079f1447466b294aa0e85141e97
kienbebong <kienpro2109@gmail.com>
2024-11-27 11:49:49 +0700

    check water

--- WaterClock/WaterClock/Controllers/Services/WaterConsumeViewController.swift
+++ WaterClock/WaterClock/Controllers/Services/WaterConsumeViewController.swift
@@ -1,17 +1,71 @@
-//
-//  WaterConsumeViewController.swift
-//  WaterClock
-//
-//  Created by Dinh Chi Kien on 7/11/24.
-//
-
 import UIKit
+import AVKit
+import Vision
+import CoreML
 
-class WaterConsumeViewController: UIViewController {
-
+class WaterConsumeViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    private var model: MLModel?
+    private let captureSession = AVCaptureSession()
+    private var resultLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemMint
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        setUpResultLabel()
     }
+    
+    
+    func setUpResultLabel() {
+        view.addSubview(resultLabel)
+        resultLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(50)
+            make.leading.equalToSuperview().offset(20)
+            make.height.equalTo(50)
+        }
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+       
+        guard let model = try? VNCoreMLModel(for: WaterModel().model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            
+          
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+            DispatchQueue.main.async {
+                self.resultLabel.text = "\(firstObservation.identifier) \(firstObservation.confidence * 100)"
+            }
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
 }
